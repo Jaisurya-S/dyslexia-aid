caeac3f9ab69b4e4ac85ea2b820f81acee907284
Surya <selvam.a9080@gmail.com>
2026-03-31 21:00:34 +0530

    Updated network host for backend

--- backend/app.py
+++ backend/app.py
@@ -1152,4 +1152,4 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"⚠ SVM Engine registration failed: {e}")
     
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
